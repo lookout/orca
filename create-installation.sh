d9c62c339dd5e9dc82a8b2a1ffe3ba739e82c1f8
Bruno Carrier <bruno.carrier@lookout.com>
2017-09-07 20:37:30 -0400

    all changes

--- create-installation.sh
+++ create-installation.sh
@@ -5,8 +5,13 @@ tar czf orca.tar.gz orca-web/build/install
 
 # now SCP the file onto your server
 # on that server, untar: `tar -xvzf orca.tar.gz`
+
+# You need to modify /etc/hosts with lines like below (maybe just 1 line is necessary.  Maybe both.  I tried both the first time and it just worked.  Of course, change the IP to whatever IP the box has):
+# 127.0.0.1 localhost localhost.localdomain ip-10-102-101-23
+# ip-10-102-101-23 localhost
+
 # `tmux` to create a new window that will survive you exiting the SSH session
-# run `orca-web/build/install/bin/orca`
+# run `orca-web/build/install/orca/bin/orca`
 # close your SSH session
 # enjoy
 
